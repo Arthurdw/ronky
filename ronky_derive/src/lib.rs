2baf4247aab93c64a19d75ed7479bee4fc17710b
Arthurdw <dev@arthurdw.com>
2025-04-12 16:53:04 +0200

    feat: added the properties setters

--- ronky_derive/src/lib.rs
+++ ronky_derive/src/lib.rs
@@ -1,11 +1,10 @@
-pub(crate) mod parsers;
 mod metadata;
+mod parsers;
 
-use parsers::parse_field;
+use parsers::{ParsedField, parse_field};
 use proc_macro::TokenStream;
-use quote::{ToTokens, quote};
+use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
-
 
 #[proc_macro]
 pub fn export_stream(input: TokenStream) -> TokenStream {
@@ -22,13 +21,30 @@ pub fn export_stream(input: TokenStream) -> TokenStream {
     };
     let metadata: proc_macro2::TokenStream = metadata::extract(&input).into();
 
-    // TODO:: parsed fields
-    let parsed_fields = fields.iter().map(parse_field);
-    // TODO: find optional fields
+    let properties = fields
+        .iter()
+        .map(parse_field)
+        .filter_map(|field| match field {
+            ParsedField::Required(field, stream) => {
+                let field_name = field.ident.as_ref().unwrap().to_string();
+                let stream: proc_macro2::TokenStream = stream.into();
+                Some(quote! {
+                    schema.set_property(#field_name, Box::new(#stream));
+                })
+            }
+            ParsedField::Optional(field, stream) => {
+                let field_name = field.ident.as_ref().unwrap().to_string();
+                let stream: proc_macro2::TokenStream = stream.into();
+                Some(quote! {
+                    schema.set_optional_property(#field_name, Box::new(#stream));
+                })
+            }
+        });
 
     quote! {
         let mut schema = ronky::PropertiesSchema::new();
         schema.set_metadata(Box::new(#metadata));
+        #(#properties)*
         schema
     }
     .into()
@@ -36,10 +52,10 @@ pub fn export_stream(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(Exported)]
 pub fn exported_derive(input: TokenStream) -> TokenStream {
+    let quotable_result: proc_macro2::TokenStream = export_stream(input.clone()).into();
+
     let input = parse_macro_input!(input as DeriveInput);
     let struct_name = input.ident.clone();
-    let result = export_stream(input.into_token_stream().into());
-    let quotable_result: proc_macro2::TokenStream = result.into();
 
     quote! {
         impl ronky::Exportable for #struct_name {
--- ronky_derive/src/parsers/mod.rs
+++ ronky_derive/src/parsers/mod.rs
@@ -1,25 +1,24 @@
-use arri_repr::{Serializable, TypeSchema, Types};
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::Field;
 
-pub(crate) struct ParsedField {
-    pub(crate) properties: Option<TokenStream>,
-    pub(crate) optional_properties: Option<TokenStream>,
+pub(crate) enum ParsedField<'a> {
+    Required(&'a Field, TokenStream),
+    Optional(&'a Field, TokenStream),
 }
 
 pub(crate) trait FieldParser {
     fn parse(&self, field: &Field) -> ParsedField;
 }
 
-pub fn parse_field(field: &Field) -> ParsedField {
-    ParsedField {
-        properties: Some(
-            quote! {
-                TypeSchema::new(Types::String)
-            }
-            .into(),
-        ),
-        optional_properties: None,
-    }
+pub fn parse_field<'a>(field: &'a Field) -> ParsedField<'a> {
+    let field_name = field.ident.as_ref().unwrap().to_string();
+    println!("Processing field: {}", field_name);
+    ParsedField::Optional(
+        field,
+        quote! {
+            ronky::TypeSchema::new(ronky::Types::String)
+        }
+        .into(),
+    )
 }
