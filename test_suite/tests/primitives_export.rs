9638c9f2f21e02bf3716114e7299ded4d3724621
Arthurdw <dev@arthurdw.com>
2025-05-03 16:56:33 +0200

    refactor: moved tests to the tests dir in the test suite

---
+++ test_suite/tests/primitives_export.rs
@@ -0,0 +1,49 @@
+use ronky::{
+    Exportable, Exported, MetadataSchema, PropertiesSchema, Serializable, TypeSchema, Types,
+};
+
+#[allow(dead_code)]
+#[derive(Exported)]
+struct TestStruct {
+    string: String,
+    string_ref: &'static str,
+    boolean: bool,
+    float32: f32,
+    float64: f64,
+    int8: i8,
+    uint8: u8,
+    int16: i16,
+    uint16: u16,
+    int32: i32,
+    uint32: u32,
+    int64: i64,
+    uint64: u64,
+}
+
+#[test]
+fn test_export() {
+    let export = TestStruct::export();
+    let mut expected = PropertiesSchema::new();
+    expected.set_metadata(
+        MetadataSchema::new()
+            .set_id("TestStruct".to_string())
+            .to_owned(),
+    );
+    expected.set_property("string", Box::new(TypeSchema::new(Types::String)));
+    expected.set_property("string_ref", Box::new(TypeSchema::new(Types::String)));
+    expected.set_property("boolean", Box::new(TypeSchema::new(Types::Boolean)));
+    expected.set_property("float32", Box::new(TypeSchema::new(Types::Float32)));
+    expected.set_property("float64", Box::new(TypeSchema::new(Types::Float64)));
+    expected.set_property("int8", Box::new(TypeSchema::new(Types::Int8)));
+    expected.set_property("uint8", Box::new(TypeSchema::new(Types::Uint8)));
+    expected.set_property("int16", Box::new(TypeSchema::new(Types::Int16)));
+    expected.set_property("uint16", Box::new(TypeSchema::new(Types::Uint16)));
+    expected.set_property("int32", Box::new(TypeSchema::new(Types::Int32)));
+    expected.set_property("uint32", Box::new(TypeSchema::new(Types::Uint32)));
+    expected.set_property("int64", Box::new(TypeSchema::new(Types::Int64)));
+    expected.set_property("uint64", Box::new(TypeSchema::new(Types::Uint64)));
+
+    assert!(export.is::<PropertiesSchema>());
+    let export = export.downcast_ref::<PropertiesSchema>().unwrap();
+    assert_eq!(*export, expected);
+}
---
+++ test_suite/tests/strict_export.rs
@@ -0,0 +1,59 @@
+use ronky::{Exportable, Exported, PropertiesSchema};
+
+#[test]
+fn test_export() {
+    #[derive(Exported)]
+    struct StrictStruct {}
+
+    let export = StrictStruct::export();
+    let expected = PropertiesSchema::new();
+
+    assert!(export.is::<PropertiesSchema>());
+    let export = export.downcast_ref::<PropertiesSchema>().unwrap();
+    assert_eq!(export.is_strict, expected.is_strict);
+}
+
+#[test]
+fn test_export_strict() {
+    #[derive(Exported)]
+    #[arri(strict)]
+    struct StrictStruct {}
+
+    let export = StrictStruct::export();
+    let mut expected = PropertiesSchema::new();
+    expected.set_strict(true);
+
+    assert!(export.is::<PropertiesSchema>());
+    let export = export.downcast_ref::<PropertiesSchema>().unwrap();
+    assert_eq!(export.is_strict, expected.is_strict);
+}
+
+#[test]
+fn test_export_strict_explicit_true() {
+    #[derive(Exported)]
+    #[arri(strict = true)]
+    struct StrictStruct {}
+
+    let export = StrictStruct::export();
+    let mut expected = PropertiesSchema::new();
+    expected.set_strict(true);
+
+    assert!(export.is::<PropertiesSchema>());
+    let export = export.downcast_ref::<PropertiesSchema>().unwrap();
+    assert_eq!(export.is_strict, expected.is_strict);
+}
+
+#[test]
+fn test_export_strict_explicit_false() {
+    #[derive(Exported)]
+    #[arri(strict = false)]
+    struct StrictStruct {}
+
+    let export = StrictStruct::export();
+    let mut expected = PropertiesSchema::new();
+    expected.set_strict(false);
+
+    assert!(export.is::<PropertiesSchema>());
+    let export = export.downcast_ref::<PropertiesSchema>().unwrap();
+    assert_eq!(export.is_strict, expected.is_strict);
+}
